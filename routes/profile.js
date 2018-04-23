12db58e046c4d28c2e0959c05ddf73b8d2ba381b
Elise Griset <elise.griset@gmail.com>
2018-04-23 17:46:04 +0200

    edit user

--- routes/profile.js
+++ routes/profile.js
@@ -4,31 +4,25 @@ const User = require('../models/User');
 
 /* GET profile page */
 
-router.get('/edit/:userId/edit', (req, res, next) => {
-  User.findById(req.params.userId)
-    .then(userDetails => {
-      res.locals.userId = req.params.userId;
-      res.locals.user = userDetails;
-      res.render('profile/edit-profile');
+router.get('/edit', (req, res, next) => {
+  res.render('profile/edit-profile');
+});
+
+// update profile informations
+
+router.post('/auth/edit-profile', (req, res, next) => {
+  const {firstName, lastName, username, email, password} = req.body;
+
+  User.findByIdAndUpdate(
+    req.user._id,
+    {firstName, lastName, username, email, password},
+    {runValidators: true}
+  )
+    .then(() => {
+      res.redirect('/map/home-page');
     })
     .catch(err => {
       next(err);
     });
 });
-
-// router.post('/process-edit/:bookId', (req, res, next) => {
-//   const {title, author, description, rating} = req.body;
-//   Book.findByIdAndUpdate(
-//     req.params.bookId, //which document to update
-//     {title, author, description, rating}, //what toupdate
-//     {runValidators: true} //extra settings
-//   )
-//     .then(() => {
-//       res.redirect(`/book/${req.params.bookId}`);
-//     })
-//     .catch(err => {
-//       next(err);
-//     });
-// });
-
 module.exports = router;
