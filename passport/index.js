10659c391da27abb3ebd01dac90065df33d80caf
Yaats <trang.billy@live.fr>
2018-04-23 17:44:57 +0200

    username affichage

--- passport/index.js
+++ passport/index.js
@@ -3,7 +3,17 @@ const passport = require('passport');
 require('./serializers');
 require('./localStrategy');
 
-module.exports = app => {
+
+function passportSetup (app) {
+  //  add properties & methods to the "req" object in routes
   app.use(passport.initialize());
   app.use(passport.session());
-};
+  app.use((req, res, next) => {
+    //  make "req.user" accesible inside hbs file as "blahUser"
+    res.locals.blahUser = req.user;
+    next();
+  })
+}
+
+
+module.exports = passportSetup;
