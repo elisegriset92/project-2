569554e352cbb70652c7dbd537925465331561e0
Elise Griset <elise.griset@gmail.com>
2018-04-24 16:42:32 +0200

    maps ok

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -62,3 +62,18 @@ axios
   .catch(err => {
     alert('something went wrong');
   });
+
+const locationInput = document.querySelector('.location-input');
+const latInput = document.querySelector('.lat-input');
+const lngInput = document.querySelector('.lng-input');
+
+const autocomplete = new google.maps.places.Autocomplete(locationInput);
+
+autocomplete.addListener('place_changed', () => {
+  const place = autocomplete.getPlace();
+  const loc = place.geometry.location;
+
+  latInput.value = loc.lat();
+  lngInput.value = loc.lng();
+  nameInput.value = loc.name();
+});
