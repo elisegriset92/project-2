4292d2ca2bef0835b037d18957d977d5930a2b99
Elise Griset <elise.griset@gmail.com>
2018-04-24 12:06:20 +0200

    map

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    console.log('IronGenerator JS imported successfully!');
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+function startMap() {
+  const paris = {
+    lat: 48.855076,
+    lng: 2.356184,
+  };
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: paris,
+  });
+}
 
-}, false);
+startMap();
